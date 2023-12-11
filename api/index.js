d1a15f5f4c0c714abc998f43c3e1531f75e0a0a4
wejdanA <lol.lol.1417@hotmail.com>
2023-12-11 06:59:11 +0300

    configure environment

--- api/index.js
+++ api/index.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const app = express();
 const productsRouter = require("../routes/productsRoutes");
+require("dotenv").config();
+const port = process.env.PORT;
+console.log("port", port);
 
 app.use("/products", productsRouter);
-
-app.listen(8080, () => {
+app.listen(port, () => {
   console.log("This server running in port 8080");
 });
